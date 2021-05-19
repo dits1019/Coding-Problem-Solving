77c59abb8e6123a944cd44aa470d1eea333570ae
dits1019 <jdits1019@gmail.com>
2021-05-19 19:09:54 +0900

    first commit

---
+++ src/baekjoon/input_output.java
@@ -0,0 +1,38 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class input_output {
+    public static void main(String[] args) {
+        // 2557. Hello World
+        // System.out.println("Hello World!");
+
+        // 10718. We love kriii
+        // System.out.print("강한친구 대한육군\n강한친구 대한육군");
+
+        // 10171. 고양이
+        // System.out.println("\\    /\\");
+        // System.out.println(" )  ( ')");
+        // System.out.println("(  /  )");
+        // System.out.print(" \\(__)|");
+
+        // 10172. 개
+        // System.out.println("|\\_/|");
+        // System.out.println("|q p|   /}");
+        // System.out.println("( 0 )\"\"\"\\");
+        // System.out.println("|\"^\"`    |");
+        // System.out.println("||_/=\\\\__|");
+
+        // 10000. A+B
+        // Scanner sc = new Scanner(System.in);
+        // int a = sc.nextInt();
+        // int b = sc.nextInt();
+        // System.out.println(a+b);
+
+        // 10001. A-B
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a-b);
+    }
+}
